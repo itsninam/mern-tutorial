d603968a61334f9bfcbdec6eb3e6f223a8791133
itsninam <madalinaoancea@hotmail.com>
2023-08-14 18:14:38 -0400

    Connect to db

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const mongoose = reuqire("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://moancea:pass123@cluster0.ngasa6d.mongodb.net/merntutorial?retryWrites=true&w=majority"
+);
 
 app.listen(3001, () => {
   console.log("SERVER RUNNING");
