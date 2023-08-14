5f449eeeef783058897bdf62f59e54564d6a2bde
itsninam <madalinaoancea@hotmail.com>
2023-08-14 18:39:46 -0400

    Creating first api end-point

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const app = express();
-const mongoose = reuqire("mongoose");
+const mongoose = require("mongoose");
+const UserModel = require("./models/Users");
 
 mongoose.connect(
   "mongodb+srv://moancea:pass123@cluster0.ngasa6d.mongodb.net/merntutorial?retryWrites=true&w=majority"
 );
+
+app.get("/getUsers", async (req, res) => {
+  //send result to frontend
+  try {
+    const users = await UserModel.find({});
+    res.send(users);
+    console.log(users);
+  } catch (err) {
+    res.send(err);
+    console.log(err);
+  }
+});
 
 app.listen(3001, () => {
   console.log("SERVER RUNNING");
