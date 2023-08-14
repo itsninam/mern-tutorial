c8e5724c49fba2263d0cd4a8c9a0bfbcf7ded8bf
itsninam <madalinaoancea@hotmail.com>
2023-08-14 18:55:45 -0400

    Import cors

--- server/index.js
+++ server/index.js
@@ -3,7 +3,10 @@ const app = express();
 const mongoose = require("mongoose");
 const UserModel = require("./models/Users");
 
+const cors = require("cors");
+
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect(
   "mongodb+srv://moancea:pass123@cluster0.ngasa6d.mongodb.net/merntutorial?retryWrites=true&w=majority"
