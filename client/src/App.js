b9cdeb8847a795a8c10e14ecc4f4ff73ae8df04e
itsninam <madalinaoancea@hotmail.com>
2023-08-15 17:40:51 -0400

    Fetch data

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
 import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/getUsers").then((data) => {
+      setUsers(data.data);
+    });
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {users.map((user) => {
+          return (
+            <div key={user.age}>
+              <p>Name: {user.name}</p>
+              <p>Age: {user.age}</p>
+              <p>Username: {user.username}</p>
+            </div>
+          );
+        })}
       </header>
     </div>
   );
