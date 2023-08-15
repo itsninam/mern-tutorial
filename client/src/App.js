7debc96b631605fc581bc06e07553c95c278c020
itsninam <madalinaoancea@hotmail.com>
2023-08-15 18:08:24 -0400

    Post request

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,32 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [userInput, setUserInput] = useState({
+    name: "",
+    age: "",
+    username: "",
+  });
 
   useEffect(() => {
     axios.get("http://localhost:3001/getUsers").then((data) => {
       setUsers(data.data);
     });
-  });
+  }, []);
+
+  const handleSubmitUser = (event) => {
+    event.preventDefault();
+
+    const newUser = {
+      name: userInput.name,
+      age: userInput.age,
+      username: userInput.username,
+    };
+
+    axios.post("http://localhost:3001/createUser", newUser).then((response) => {
+      console.log("User created");
+      setUsers([...users, newUser]);
+    });
+  };
 
   return (
     <div className="App">
@@ -25,6 +45,34 @@ function App() {
           );
         })}
       </header>
+
+      <form action="" onSubmit={handleSubmitUser}>
+        <input
+          type="text"
+          placeholder="Enter name"
+          value={userInput.name}
+          onChange={(event) =>
+            setUserInput({ ...userInput, name: event.target.value })
+          }
+        />
+        <input
+          type="number"
+          placeholder="Enter age"
+          value={userInput.age}
+          onChange={(event) =>
+            setUserInput({ ...userInput, age: event.target.value })
+          }
+        />
+        <input
+          type="text"
+          placeholder="Enter username"
+          value={userInput.username}
+          onChange={(event) =>
+            setUserInput({ ...userInput, username: event.target.value })
+          }
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
